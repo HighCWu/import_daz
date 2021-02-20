ccce762e4b3ebf00151374902c8772e8063abfa8
Thomas Comsol <thomas_larsson_01@hotmail.com>
2021-02-20 03:03:07 +0100

    Don't copy vertex group to HD mesh if the number of verts is wrong.

--- modifier.py
+++ modifier.py
@@ -456,7 +456,11 @@ class SkinBinding(Modifier):
         makeArmatureModifier(self.name, context, ob, rig)
         self.addVertexGroups(ob, geonode, rig)
         hdob = geonode.hdobject
-        if hdob and hdob != ob and hdob.DazMultires and GS.useMultires:
+        if (hdob and
+            hdob != ob and
+            len(hdob.data.vertices) == len(ob.data.vertices) and
+            hdob.DazMultires and
+            GS.useMultires):
             hdob.parent = ob.parent
             makeArmatureModifier(self.name, context, hdob, rig)
             copyVertexGroups(ob, hdob)
