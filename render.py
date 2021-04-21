6c7363ba9ee83b4873fc679d2be30d082cc38d60
Thomas Comsol <thomas_larsson_01@hotmail.com>
2021-04-21 05:03:35 +0200

    Fix for issue 473: Work on backgrounds

--- render.py
+++ render.py
@@ -31,6 +31,7 @@ from .asset import Asset
 from .channels import Channels
 from .material import Material, WHITE, isBlack
 from .cycles import CyclesMaterial, CyclesTree
+from .cgroup import CyclesGroup
 from .utils import *
 
 #-------------------------------------------------------------
@@ -47,14 +48,17 @@ class RenderOptions(Asset, Channels):
 
 
     def initSettings(self, settings, backdrop):
+        if "backdrop_visible" in settings.keys():
+            if not settings["backdrop_visible"]:
+                return
+        if "backdrop_visible_in_render" in settings.keys():
+            if not settings["backdrop_visible_in_render"]:
+                return
+        if backdrop:
+            self.backdrop = backdrop
         for key,value in settings.items():
             if key == "background_color":
                 self.background = value
-            if ("backdrop_visible" in settings.keys() and
-                backdrop and
-                settings["backdrop_visible"] and
-                settings["backdrop_visible_in_render"]):
-                self.backdrop = backdrop
 
 
     def __repr__(self):
@@ -107,12 +111,9 @@ class WorldMaterial(CyclesMaterial):
             return
         mode = self.getValue(["Environment Mode"], 3)
         # [Dome and Scene, Dome Only, Sun-Skies Only, Scene Only]
-        if mode is None:
+        if mode == 3:
+            print("Scene Only")
             return
-
-        self.refractive = False
-        Material.build(self, context)
-        self.tree = WorldTree(self)
 
         self.envmap = self.getChannel(["Environment Map"])
         fixray = False
@@ -120,7 +121,7 @@ class WorldMaterial(CyclesMaterial):
         if mode in [0,1] and self.envmap:
             print("Draw environment", mode)
             if not self.getValue(["Draw Dome"], False):
-                print("Don't draw environment. Draw Dome turned off")
+                print("Draw Dome turned off")
             elif self.getImageFile(self.envmap) is None:
                 print("Don't draw environment. Image file not found")
             else:
@@ -135,6 +136,10 @@ class WorldMaterial(CyclesMaterial):
         if not foundenv:
             print("Dont draw environment. Environment mode == %d" % mode)
             return
+
+        self.refractive = False
+        Material.build(self, context)
+        self.tree = WorldTree(self)
 
         world = self.rna = bpy.data.worlds.new(self.name)
         world.use_nodes = True
@@ -152,6 +157,24 @@ class WorldMaterial(CyclesMaterial):
 #   World Tree
 #-------------------------------------------------------------
 
+class BackgroundGroup(CyclesGroup):
+    def __init__(self):
+        CyclesGroup.__init__(self)
+        self.insockets += ["Color"]
+        self.outsockets += ["Fac", "Color"]
+
+    def create(self, node, name, parent):
+        CyclesGroup.create(self, node, name, parent, 2)
+        self.group.inputs.new("NodeSocketColor", "Color")
+        self.group.outputs.new("NodeSocketFloat", "Fac")
+        self.group.outputs.new("NodeSocketColor", "Color")
+
+    def addNodes(self, args=None):
+        lightpath = self.addNode("ShaderNodeLightPath", 1)
+        self.links.new(lightpath.outputs["Is Camera Ray"], self.outputs.inputs["Fac"])
+        self.links.new(self.inputs.outputs["Color"], self.outputs.inputs["Color"])
+
+
 class WorldTree(CyclesTree):
 
     def __init__(self, wmat):
@@ -162,66 +185,86 @@ class WorldTree(CyclesTree):
     def build(self):
         from mathutils import Euler, Matrix
 
-        background = self.material.background
         backdrop = self.material.backdrop
+        background = self.material.background
         envmap = self.material.envmap
-        eeveefix = False
-        tex = None
+        strength = 1
+        color = WHITE
         if envmap:
-            self.makeTree(slot="Generated")
-            rot = self.getValue(["Dome Rotation"], 0)
-            orx = self.getValue(["Dome Orientation X"], 0)
-            ory = self.getValue(["Dome Orientation Y"], 0)
-            orz = self.getValue(["Dome Orientation Z"], 0)
-
-            if rot != 0 or orx != 0 or ory != 0 or orz != 0:
-                mat1 = Euler((0,0,-rot*D)).to_matrix()
-                mat2 = Euler((0,-orz*D,0)).to_matrix()
-                mat3 = Euler((orx*D,0,0)).to_matrix()
-                mat4 = Euler((0,0,ory*D)).to_matrix()
-                mat = mat1 @ mat2 @ mat3 @ mat4
-                self.addMapping(mat.to_euler())
-
-            color = WHITE
-            value = self.material.getChannelValue(envmap, 1)
-            img = self.getImage(envmap, "NONE")
-            if img:
-                tex = self.addTexEnvNode(img, "NONE")
-                self.links.new(self.texco, tex.inputs["Vector"])
-            strength = self.getValue(["Environment Intensity"], 1) * value
+            tex, strength = self.buildEnvmap(envmap)
         elif backdrop:
-            self.makeTree(slot="Window")
-            strength = 1
-            color = background
-            eeveefix = True
-            img = self.getImage(backdrop, "COLOR")
-            if img:
-                tex = self.addTextureNode(2, img, img.name, "COLOR")
-                self.linkVector(self.texco, tex)
+            tex = self.buildBackdrop(backdrop)
         else:
-            self.makeTree()
-            strength = 1
-            color = background
-            eeveefix = True
             tex = None
+            self.makeTree()
 
-        bg = self.addNode("ShaderNodeBackground", 4)
+        self.column += 1
+        bg = self.addNode("ShaderNodeBackground")
         bg.inputs["Strength"].default_value = strength
         self.linkColor(tex, bg, color)
-        output = self.addNode("ShaderNodeOutputWorld", 6)
-        if eeveefix:
-            lightpath = self.addNode("ShaderNodeLightPath", 4)
-            mix = self.addNode("ShaderNodeMixShader", 5)
-            self.links.new(lightpath.outputs[0], mix.inputs[0])
-            self.links.new(bg.outputs[0], mix.inputs[2])
+
+        if background:
+            dazbg = self.addGroup(BackgroundGroup, "DAZ Background")
+            dazbg.inputs["Color"].default_value[0:3] = background
+            self.column += 1
+            mix = self.addNode("ShaderNodeMixShader")
+            self.links.new(dazbg.outputs["Fac"], mix.inputs[0])
+            self.links.new(bg.outputs["Background"], mix.inputs[1])
+            self.links.new(dazbg.outputs["Color"], mix.inputs[2])
+
+        self.column += 1
+        output = self.addNode("ShaderNodeOutputWorld")
+        if background:
             self.links.new(mix.outputs[0], output.inputs["Surface"])
         else:
-            self.links.new(bg.outputs[0], output.inputs["Surface"])
+            self.links.new(bg.outputs["Background"], output.inputs["Surface"])
         self.prune()
 
 
+    def buildEnvmap(self, envmap):
+        self.makeTree(slot="Generated")
+        self.column = 1
+        rot = self.getValue(["Dome Rotation"], 0)
+        orx = self.getValue(["Dome Orientation X"], 0)
+        ory = self.getValue(["Dome Orientation Y"], 0)
+        orz = self.getValue(["Dome Orientation Z"], 0)
+
+        if rot != 0 or orx != 0 or ory != 0 or orz != 0:
+            mat1 = Euler((0,0,-rot*D)).to_matrix()
+            mat2 = Euler((0,-orz*D,0)).to_matrix()
+            mat3 = Euler((orx*D,0,0)).to_matrix()
+            mat4 = Euler((0,0,ory*D)).to_matrix()
+            mat = mat1 @ mat2 @ mat3 @ mat4
+            self.addMapping(mat.to_euler())
+
+        value = self.material.getChannelValue(envmap, 1)
+        img = self.getImage(envmap, "NONE")
+        if img:
+            self.column += 1
+            tex = self.addNode("ShaderNodeTexEnvironment")
+            self.setColorSpace(tex, "NONE")
+            if img:
+                tex.image = img
+                tex.name = img.name
+            self.links.new(self.texco, tex.inputs["Vector"])
+        strength = self.getValue(["Environment Intensity"], 1) * value
+        return tex, strength
+
+
+    def buildBackdrop(self, backdrop):
+        self.makeTree(slot="Window")
+        self.column = 1
+        img = self.getImage(backdrop, "COLOR")
+        if img:
+            self.column += 1
+            tex = self.addTextureNode(self.column, img, img.name, "COLOR")
+            self.linkVector(self.texco, tex)
+        return tex
+
+
     def addMapping(self, rot):
-        mapping = self.addNode("ShaderNodeMapping", 2)
+        self.column += 1
+        mapping = self.addNode("ShaderNodeMapping")
         mapping.vector_type = 'TEXTURE'
         if hasattr(mapping, "rotation"):
             mapping.rotation = rot
@@ -238,15 +281,6 @@ class WorldTree(CyclesTree):
         if img is None:
             img = asset.buildCycles(colorSpace)
         return img
-
-
-    def addTexEnvNode(self, img, colorSpace):
-        tex = self.addNode("ShaderNodeTexEnvironment", 2)
-        self.setColorSpace(tex, colorSpace)
-        if img:
-            tex.image = img
-            tex.name = img.name
-        return tex
 
 #-------------------------------------------------------------
 #
